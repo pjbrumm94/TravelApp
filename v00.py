a4522f4d3dde021d4cd79419dd3b616ce6200bd2
pjbrumm94 <pjbrumm@gmail.com>
2025-04-04 15:04:19 -0700

    converted script to functions

--- v00.py
+++ v00.py
@@ -2,16 +2,25 @@ import pandas as pd
 from openai import OpenAI
 client  = OpenAI()
 
-csv_file = "data/example.csv"
+def loadGS(name, id):
+    url = f"https://docs.google.com/spreadsheets/d/{id}/gviz/tq?tqx=out:csv&sheet={name}"
+    return(pd.read_csv(url))
 
-if csv_file:
-    df = pd.read_csv(csv_file)
-            
+def llm_prompt(data):
     # Call OpenAI
-    prompt = f"Create a combined travel itinerary combining this information: {df}"
+    prompt = f"Create a combined travel itinerary combining this information:{data}"
     response = client.responses.create(
         model="gpt-4o",
         input=prompt
         )
-    itinerary = response.output_text
+    return(response.output_text)
+
+# Main Script
+sheet_name  = 'TravelCriteria'
+sheet_id    = '1q-xznX5gGTMO9eEcYhJl_uMiKdOuju9A3tujfZvdXlU'
+
+# Load Google Sheet into DataFrame
+data        = loadGS(sheet_name, sheet_id)
+# Send Google Sheet data to llm prompt
+itinerary   = llm_prompt(data)
 print(itinerary)
