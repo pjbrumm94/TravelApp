2e6d7350b8ed0855df32d8f0c8a9647d08205d75
pjbrumm94 <pjbrumm@gmail.com>
2025-04-04 18:51:38 -0700

    update

--- v00.py
+++ v00.py
@@ -6,6 +6,11 @@ def loadGS(name, id):
     url = f"https://docs.google.com/spreadsheets/d/{id}/gviz/tq?tqx=out:csv&sheet={name}"
     return(pd.read_csv(url))
 
+def unpack_df(info):
+    
+    # variables
+    return()
+
 def llm_prompt(data):
     # Call OpenAI
     prompt = f"Create a combined travel itinerary combining this information:{data}"
@@ -15,7 +20,10 @@ def llm_prompt(data):
         )
     return(response.output_text)
 
-if __name__ = '__main__':
+def llm_promp_revision(data, new_options):
+
+
+if __name__ == '__main__':
     sheet_name  = 'TravelCriteria'
     sheet_id    = '1q-xznX5gGTMO9eEcYhJl_uMiKdOuju9A3tujfZvdXlU'
 
